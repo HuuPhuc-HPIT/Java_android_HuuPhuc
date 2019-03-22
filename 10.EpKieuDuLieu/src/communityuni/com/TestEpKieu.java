6e7103866f3d28b4096d651f7e567c364311f3ca
hpcnttit <48825889+hpcnttit@users.noreply.github.com>
2019-03-22 20:19:52 +0700

    first commit

---
+++ 10.EpKieuDuLieu/src/communityuni/com/TestEpKieu.java
@@ -0,0 +1,18 @@
+package communityuni.com;
+
+public class TestEpKieu {
+
+	public static void main(String[] args) {
+		System.out.println(1/2);
+		System.out.println(1.0/2);
+		System.out.println((double)1/2);
+		
+		// double x = 10/3;
+		// double x = 10.0/3;
+		// double x = 10/3.0;
+		double x = (double)10/3;
+		System.out.println("x = " + x);
+		
+	}
+
+}
---
+++ Hello/src/communityuni/com/HelloWorld.java
@@ -0,0 +1,8 @@
+package communityuni.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	}
+}
