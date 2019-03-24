abe603ef296994e16484e93a8cde292f71b8a34d
hpcnttit <48825889+hpcnttit@users.noreply.github.com>
2019-03-24 15:48:14 +0700

    add

---
+++ 53.Mang_LuyenTap/src/hpit/com/TestLuyenTap.java
@@ -0,0 +1,176 @@
+package hpit.com;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TestLuyenTap {
+	static int []M; 	
+	public static void menu() {
+		System.out.println("1. Nhập mảng");
+		System.out.println("2. Xuất mảng");
+		System.out.println("3. Tổng mảng");
+		System.out.println("4. Tìm kiếm");
+		System.out.println("5.Lớn nhất");
+		System.out.println("6. Nhỏ nhất");
+		System.out.println("7. Số nguyên tố");
+		System.out.println("8. Xếp tăng dần");
+		System.out.println("9. Xếp giảm dần");
+		System.out.println("10. Thoat");
+		System.out.println("Mời bạn chọn: ");
+		
+		int chon= new Scanner(System.in).nextInt();
+		switch(chon) {
+		case 1: 
+			nhapMang();
+			break;
+		case 2:
+			xuatMang();
+			break;
+		case 3: 
+			tinhTong();
+			break;
+		case 4:
+			timKiem();
+			break;
+		case 5:
+			timMax();
+			break;
+		case 6:
+			timMin();
+			break;
+		case 7:
+			soNguyenTo();
+			break;
+		case 8:
+			xepTangDan();
+			break;
+		case 9:
+			xepGiamDan();
+			break;
+		case 10:
+			System.err.println("Tks ban da dung phan mem");
+			System.exit(0);
+		default:
+			System.out.println("Ban nhap sai vung menu: ");
+			break;
+		}
+		
+	}
+	private static void timKiem() {
+		// TODO Auto-generated method stub
+		System.out.println("Mời bạn nhâp số cần tìm:");
+		int k =new Scanner(System.in).nextInt();
+		int soLan=0;
+		String s="";
+		for (int i=0; i<M.length; i++) {
+			if(M[i]==k) {
+				soLan++;
+				s+= i+";";
+			}
+		}
+		if(soLan==0) {
+			System.out.println("Không tìm thấy "+k+" trong mảng!");
+		}else {
+			System.out.println(k+" xuất hiện trong mảng với các vị trí: "+s);
+		}
+	}
+	private static void tinhTong() {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for (int x: M) {
+			sum +=x;
+		}
+		System.out.println("Tổng bằng: "+sum);
+	}
+	private static void nhapMang() {
+		// TODO Auto-generated method stub
+		System.out.println("Mời bạn nhập số phần tử: ");
+		int n = new Scanner(System.in).nextInt();
+		M = new int[n];
+		Random rd= new Random();
+		for (int i=0; i<M.length; i++) {
+			M[i] = rd.nextInt(100);
+			
+		}
+	}
+	private static void xuatMang() {
+		// TODO Auto-generated method stub
+		System.out.println("Các phàn tử trong mảng là: ");
+		for(int i=0; i<M.length; i++) {
+			System.out.print(M[i]+"\t");
+		}
+		System.out.println();
+	}
+	private static void xepGiamDan() {
+		// TODO Auto-generated method stub
+		for(int i=0; i<M.length; i++) {
+			for(int j=i+1; j<M.length; j++) {
+				if(M[i]<M[j]) {
+					int tam=M[i];
+					M[i]=M[j];
+					M[j]=tam;
+				}
+			}
+		}
+	}
+	private static void xepTangDan() {
+		// TODO Auto-generated method stub
+		for(int i=0; i<M.length; i++) {
+			for(int j=i+1; j<M.length; j++) {
+				if(M[i]>M[j]) {
+					int tam=M[i];
+					M[i]=M[j];
+					M[j]=tam;
+				}
+			}
+		}
+	}
+	private static void soNguyenTo() {
+		// TODO Auto-generated method stub
+		System.out.println("Các số nguyên tố: ");
+		for(int i=0; i<M.length; i++) {
+			int dem=0;
+			for(int j=1; j<=M[i]; j++) {
+				if(M[i]%j==0) {
+					dem++;
+				}
+			}
+			if(dem==2) {
+				System.out.print(M[i]+"\t");
+			}
+		}
+		System.out.println();
+	}
+	private static void timMin() {
+		// TODO Auto-generated method stub
+		int min=M[0];
+		for(int i=1; i<M.length; i++) {
+			if(M[i]<min) {
+				min=M[i];
+			}
+		}
+		System.out.println("Số nhỏ nhất là: "+min);
+	}
+	private static void timMax() {
+		// TODO Auto-generated method stub
+		int max=M[0];
+		for(int i=1; i<M.length; i++) {
+			if(M[i]>max) {
+				max=M[i];
+			}
+		}
+		System.out.println("Số lớn nhất bằng "+max);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+			menu();
+			System.out.println();
+		}
+	}
+
+}
---
+++ 59.HashMap_LuyenTap/src/hpit/com/TestRenLuyen.java
@@ -0,0 +1,96 @@
+package hpit.com;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class TestRenLuyen {
+	static HashMap<Integer, String> map= new HashMap<Integer, String>();
+	public static void menu() {
+		System.out.println("1. Thêm");
+		System.out.println("2. Xuất");
+		System.out.println("3. Sửa");
+		System.out.println("4. Xóa ");
+		System.out.println("5. Tìm kiếm");
+		System.out.println("6. Thoát");
+		System.out.print("Mời bạn chọn: ");
+		int chon= new Scanner(System.in).nextInt();
+		switch(chon) {
+		case 1:
+			them();
+			break;
+		case 2:
+			xuat();
+			break;
+		case 3:
+			sua();
+			break;
+		case 4:
+			xoa();
+			break;
+		case 5:
+			tim();
+			break;
+		case 6:
+			System.err.println("Tks!");
+			System.exit(0);
+			break;
+		default:
+			System.out.println("Ban nhap sai! Vui long nhap lai!");
+			break;
+		}
+	}
+	private static void tim() {
+		//Lọc tất cả cuốn sách có từ "java"
+		System.out.print("Nhập tên sách muôn tìm: ");
+		String ten= new Scanner(System.in).nextLine();
+		for(Entry<Integer, String> item: map.entrySet()) {
+			if(item.getValue().contains(ten)) {
+				System.out.println(item.getKey()+"-"+item.getValue());
+				
+			}
+		}
+	}
+	private static void xuat() {
+		
+		System.out.println("Mã \t Tên sách");
+		for(Entry<Integer, String> item : map.entrySet()) {
+			System.out.println(item.getKey()+" \t "+item.getValue());
+		}
+	}
+	private static void xoa() {
+		System.out.print("Mời bạn nhập mã muốn xóa: ");
+		int ma= new Scanner(System.in).nextInt();
+		if(map.containsKey(ma)==false) {
+			System.out.println("Mã k đúng");
+		}else {
+			map.remove(ma);
+		}
+	}
+	private static void sua() {
+		System.out.print("Mời bạn nhập mã muốn sửa: ");
+		int ma= new Scanner(System.in).nextInt();
+		if(map.containsKey(ma)==false) {
+			System.out.println("Mã" +ma+ "không tồn tại!");
+		}else {
+			System.out.print("Nhập tên sách mới: ");
+			String ten= new Scanner(System.in).nextLine();
+			map.put(ma, ten);
+		}
+	}
+	private static void them() {
+		System.out.println("Nhập mã sách:");
+		int ma= new Scanner(System.in).nextInt();
+		System.out.println("Nhập tên sách: ");
+		String ten= new Scanner(System.in).nextLine();
+		if(map.containsKey(ma)==false) {
+			map.put(ma, ten);		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+			menu();
+		}
+	}
+
+}
